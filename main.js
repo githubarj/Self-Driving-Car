13ca93162c8ea96650e0720de265dbee5970d362
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-07-19 16:45:21 +0300

    visual representation of the neural network

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const traffic=[
 
 animate();
 
-function animate(){
+function animate(time){
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
@@ -34,5 +34,7 @@ function animate(){
 
     carCtx.restore();
 
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,car.brain);
     requestAnimationFrame(animate);
 }
